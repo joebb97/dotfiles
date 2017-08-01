ce07a38c269159f68a96771620a0063a4e9141dd
Joseph Buiteweg <joseph.buiteweg@soartech.com>
2017-08-01 13:38:18 -0400

    Added bash_it

--- .bashrc
+++ .bashrc
@@ -4,4 +4,5 @@ export LSCOLORS=ExFxBxDxCxegedabagacad
 alias ls='ls -GFh -a'
 alias connectumich='ssh joebb@login-course.engin.umich.edu'
 alias debug='sudo gdb'
+alias sbrc='source ~/.bashrc'
 source /usr/share/autojump/autojump.sh
--- setup.sh
+++ setup.sh
@@ -35,6 +35,11 @@ then
     echo "source /usr/share/autojump/autojump.sh" >> ~/.bashrc
 fi
 
+if [ ! -d ~/.bash_it ]
+then
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+    ~/.bash_it/install.sh
+fi
+
 echo "sourcing bashrc"
 source ~/.bashrc
-
