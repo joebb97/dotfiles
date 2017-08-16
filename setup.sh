969d864a46b79e645d97568aa857099803efb083
Joseph Buiteweg <joseph.buiteweg@soartech.com>
2017-08-16 14:01:53 -0400

    Updated setup script, added Vundle to vimrc

--- setup.sh
+++ setup.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-printf "running dotfiles setup at $(date)\n"
+printf "running dotfiles setup at $(date)...\n"
 if [ "$1" == "-c" ] || [ "$1" == "--copy" ]
 then
-    files=$(ls -a | grep '^\.\w' | grep -v '^\.git$' | grep -v '\.swp$')
+    files=".vimrc .tmux.conf .bashrc .gitconfig .inputrc"
+    backup_dir = "~/dotfile-backups"
     for file in $files
     do
         if [ -f ~/$file ]
         then
-            if [ ! -d ~/dot-file-backups ]
+            if [ ! -d $backup_dir]
             then
-                mkdir ~/dot-file-backups
+                mkdir $backup_dir
             fi
 
-            if [ ! -f ~/dot-file-backups/$file ]
+            if [ ! -f $backup_dir/$file ]
             then
-                cp -ivb ~/$file ~/dot-file-backups/$file
+                printf "Moving $file to $backup_dir...\n"
+                mv -ivb ~/$file $backup_dir/$file
             fi   
             
+            printf "Making symlink from $(pwd)/$file to ~/$file...\n"
             ln -sivb $(pwd)/$file ~/$file # symbolic links require full path
         fi
     done
@@ -24,13 +27,13 @@ fi
 
 if [ ! -d ~/.tmux ]
 then
-    printf "cloning tmux plugin manager\n"
+    printf "cloning tmux plugin manager...\n"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 if [ ! -d /usr/share/autojump ]
 then
-    printf "installing autojump\n"
+    printf "installing autojump...\n"
     sudo apt install autojump
     echo "source /usr/share/autojump/autojump.sh" >> ~/.bashrc
 fi
@@ -41,5 +44,16 @@ then
     ~/.bash_it/install.sh
 fi
 
+if [ ! -d ~/.vim/bundle/Vundle.vim ]
+then
+    printf "cloning Vundle (vim plugin manager)...\n"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+if [ ! $(dpkg -l | grep 'vim-gtk') ]
+then
+    sudo apt install vim-gtk
+fi
+    
 echo "sourcing bashrc"
 source ~/.bashrc
