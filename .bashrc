ad7c70fbadc3526934acf213dd9b1ce7d02350bb
joebb <joebb@umich.edu>
2018-06-08 16:46:25 -0400

    Copied some bidness

---
+++ .bashrc
@@ -0,0 +1,6 @@
+export BASH_IT="$HOME/.bash_it"
+export BASH_IT_THEME='bakke'
+export SCM_CHECK=true
+export PATH="$HOME/.cargo/bin:$PATH"
+
+source "$BASH_IT"/bash_it.sh
---
+++ custom.aliases.bash
@@ -0,0 +1,6 @@
+alias vim=/usr/local/Cellar/vim/8.0.1800/bin/vim
+alias make=/usr/local/bin/make
+alias m='make'
+alias sbrc='source $HOME/.bashrc'
+alias ebrc="vim $HOME/.bashrc"
+alias evrc="vim $HOME/.vimrc"
