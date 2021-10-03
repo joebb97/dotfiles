589b4b9cbf342474fedd9ae551c8ddb4002946f8
Joey Buiteweg <joebb@umich.edu>
2021-10-03 13:07:07 -0400

    Installing packages works for Debian

--- .extra/install/install.go
+++ .extra/install/install.go
@@ -4,17 +4,46 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 type distroGroup struct {
-	names   []string
-	manager string
+	names      []string
+	manager    string
+	installCmd []string
+	updateCmd  string
+}
+
+func (d *distroGroup) run(args ...string) error {
+	asStr := fmt.Sprint(args)
+	fmt.Println("Running", d.manager, asStr[1:len(asStr)-1])
+	err := exec.Command(d.manager, args...).Run()
+	return err
+}
+
+func (d *distroGroup) runUpdate() error {
+	return d.run(d.updateCmd)
+}
+
+func (d *distroGroup) runInstall(packs ...string) error {
+	args := append(d.installCmd, packs...)
+	return d.run(args...)
 }
 
 var distroGroups = []distroGroup{
-	{names: []string{"Pop", "Ubuntu", "Mint"}, manager: "apt"},
-	{names: []string{"ManjaroLinux", "ArchLinux"}, manager: "pacman"},
+	{
+		names:      []string{"Debian", "Pop", "Ubuntu", "Mint"},
+		manager:    "apt-get",
+		installCmd: []string{"install", "-y"},
+		updateCmd:  "update",
+	},
+	{
+		names:      []string{"Arch", "ManjaroLinux", "ArchLinux"},
+		manager:    "pacman",
+		installCmd: []string{"-S"},
+		updateCmd:  "-Syu",
+	},
 }
 
 const distroFilename = "/etc/lsb-release"
@@ -45,7 +74,23 @@ func getDistroGroup() *distroGroup {
 	return nil
 }
 
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
-	distro := getDistroGroup()
-	fmt.Println(distro.names, distro.manager)
+	d := getDistroGroup()
+	if d != nil {
+		fmt.Println("Distro family:", d.names[0])
+		fmt.Println("Distro package manager:", d.manager)
+	} else {
+		fmt.Println("no distro info available")
+		return
+	}
+	checkErr(d.runUpdate())
+	toInstall := []string{"git", "vim"}
+	checkErr(d.runInstall(toInstall...))
 }
