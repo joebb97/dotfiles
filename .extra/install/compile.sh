52487ff7abf16107e29bb234ba231b3e74a88c3e
Joseph Buiteweg <joebb@umich.edu>
2021-10-11 14:53:11 -0400

    Make compile.sh more non-root friendly

--- .extra/install/compile.sh
+++ .extra/install/compile.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 set -eu
 
+_def_prefix="/usr/local"
 _to_install=$1
-if [ -f "$_to_install" ]; then
-    echo "Invoke this script on a directory (you invoked on a file)"
-elif [ -d "$_to_install" ]; then
-    cd "$_to_install" || exit
-    if [ "$(basename "$_to_install")" = "vim" ]; then
-        echo "It's vim!"
-        _configure_cmd="./configure --with-features=huge
-                                    --enable-pythoninterp
-                                    --enable-python3interp
-                                    --enable-multibyte
-                                    --enable-terminal | tee /tmp/log.txt"
-        apt install -y gcc make python3-dev libncurses-dev
-        $_configure_cmd
-        sudo make -j 8
-        sudo make install
-    elif [ "$(basename "$_to_install")" = "autojump" ]; then
-        echo "It's autojump!"
-        ./install.py
-    else
-        apt install -y autotools-dev autoconf automake pkg-config
-        if [ -f "autogen.sh" ]; then
-            ./autogen.sh
-        fi
-        ./configure
-        make -j 8
-        make -j 8 install
-    fi
+[ $# -eq 2 ] && _prefix=$2 || _prefix="$_def_prefix"
+[ "$(id -u)" -eq 0 ] && _isroot=true || _isroot=false
+[ "$(basename "$_prefix")" = "bin" ] && echo "Prefix shouldn't end in bin" && exit 1
+
+[ "$_isroot" = false ] && [ "$_prefix" = "$_def_prefix" ] && 
+    echo "Need to be root to install to $_def_prefix" && exit 1
+
+[ -f "$_to_install" ] && echo "Invoke this script on a directory (you invoked on a file)" && exit 1
+[ ! -d "$_to_install" ]  && echo "Not anything we can install, whoops" && exit 1
+
+cd "$_to_install" || exit
+if [ "$(basename "$_to_install")" = "vim" ]; then
+    echo "It's vim!"
+    _configure_cmd="./configure --with-features=huge
+                                --prefix=$_prefix
+                                --enable-python3interp
+                                --enable-multibyte
+                                --enable-terminal"
+    [ "$_isroot" = true ] && which apt-get && apt-get install -y gcc make python3-dev libncurses-dev
+    $_configure_cmd
+    make -j 8
+    make -j 8 install
+elif [ "$(basename "$_to_install")" = "autojump" ]; then
+    echo "It's autojump!"
+    ./install.py
 else
-    echo "Not anything we can install, whoops"
+    [ "$_isroot" = true ] && which apt-get && apt-get install -y autotools-dev autoconf automake pkg-config
+    [ -f "autogen.sh" ] && ./autogen.sh
+    ./configure --prefix="$_prefix"
+    make -j 8
+    make -j 8 install
 fi
